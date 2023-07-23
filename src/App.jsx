ee76394da44ae31daa703ecc7d41e441e8367b47
CodervsFixer <codervsfixer.com>
2023-07-24 00:55:47 +0600

    components create file

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
 
   return (
-    <>
+    <div className='container'>
       <h1 className='text-center fw-bold'>User Information Collect Step By Step</h1>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/FormPersonalDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const FormPersonalDetails = () => {
+  return (
+    <div>FormPersonalDetails</div>
+  )
+}

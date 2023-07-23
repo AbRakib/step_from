73af6b925374f3ae25ac50920400b068280dd50d
CodervsFixer <codervsfixer.com>
2023-07-24 00:49:50 +0600

    Start From Step Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center fw-bold'>User Information Collect Step By Step</h1>
+    </>
+  )
+}
+
+export default App
